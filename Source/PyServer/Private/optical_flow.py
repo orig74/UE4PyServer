94d56539a14cdf585a0673d6d5770a261d704271
ori ganoni <ori.ganoni@pg.canterbury.ac.nz>
2016-10-07 17:14:40 +1300

    demo work on world data

--- Source/PyServer/Private/optical_flow.py
+++ Source/PyServer/Private/optical_flow.py
@@ -16,7 +16,7 @@ class optical_flow_track(object):
                    criteria = (cv2.TERM_CRITERIA_EPS | cv2.TERM_CRITERIA_COUNT, 10, 0.03))
 
         # Create some random colors
-        self.color = np.random.randint(0,255,(100,3))
+        self.color = np.random.randint(0,255,(2000,3))
         self.old_gray=None
         self.old_frame=None
         self.p0=self.p1=None
@@ -25,25 +25,32 @@ class optical_flow_track(object):
         if self.old_gray is None:
             self.old_frame=frame
             self.old_gray=cv2.cvtColor(self.old_frame, cv2.COLOR_BGR2GRAY)
-            self.p0 = cv2.goodFeaturesToTrack(self.old_gray, mask = None, **feature_params)
-            self.mask=np.zeros_like(self.old_frame)
+            #self.p0 = cv2.goodFeaturesToTrack(self.old_gray, mask = None, **self.feature_params)
+            self.p0 = np.array([(i,j) for i in range(0,frame.shape[1],30) for j in range(0,frame.shape[0],30)],dtype='float32').reshape(-1,1,2)
 
-        self.frame_gray = cv2.cvtColor(self.frame, cv2.COLOR_BGR2GRAY)
-        p1, st, err = cv2.calcOpticalFlowPyrLK(self.old_gray, self.frame_gray, self.p0, None, **lk_params)
+        #import ipdb;ipdb.set_trace()
+        frame_gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+        p1, st, err = cv2.calcOpticalFlowPyrLK(self.old_gray, frame_gray, self.p0, None, **self.lk_params)
 
         # Select good points
         good_new = p1[st==1]
-        good_old = p0[st==1]
+        good_old = self.p0[st==1]
 
         # draw the tracks
         for i,(new,old) in enumerate(zip(good_new,good_old)):
              a,b = new.ravel()
              c,d = old.ravel()
-             mask = cv2.line(mask, (a,b),(c,d), color[i].tolist(), 2)
-             ret_frame = cv2.circle(frame,(a,b),5,color[i].tolist(),-1)
-        img = cv2.add(ret_frame,self.mask)
+             frame = cv2.circle(frame,(a,b),2,self.color[i].tolist(),-1)
 
         # Now update the previous frame and previous points
-        old_gray = frame_gray.copy()
-        p0 = good_new.reshape(-1,1,2)
+        self.old_gray = frame_gray.copy()
+        self.p0 = good_new.reshape(-1,1,2)
+        return frame 
 
+if __name__=="__main__":
+    frame=cv2.imread('/tmp/screenshot.png')
+    of=optical_flow_track()
+    out=of.feed(frame)
+    cv2.imshow('cv window',out)
+    cv2.waitKey(0)
+        
--- Source/PyServer/Private/track_test.py
+++ Source/PyServer/Private/track_test.py
@@ -1,6 +1,10 @@
 # vim: tabstop=8 expandtab shiftwidth=4 softtabstop=4
 from Wrappers import phandlers as ph
-import cv2
+import cv2,imp
+import optical_flow
+from optical_flow import optical_flow_track
+imp.reload(optical_flow)
+
 print('---- track_test imported ----')
 print('---- destroy cv windows ----')
 cv2.destroyAllWindows()
@@ -10,19 +14,19 @@ cv2.waitKey(1)
 def main_loop(gworld):
     iter=0
     print('-------> start main_loop 2')
+    of=optical_flow_track()
     #cv2.destroyAllWindows()
+    camera_actor=ph.FindActorByName(gworld,'CameraActor_2',1) 
+    if camera_actor is None:
+        print('could not find CameraActor_2 yeilding forever')
+        while 1:
+            yield
+    tick_actor=ph.FindActorByName(gworld,'PyServerTickActor_0',1)
+    ph.MoveToCameraActor(tick_actor,camera_actor)
     while 1:
         #print('-------> main_loop',iter)
         yield
         iter+=1
-        if iter==100:
-            import imp; imp.reload(ph)
-            #import ipdb;ipdb.set_trace()
-            aname='PlayerController_0'
-            tick_actor=ph.FindActorByName(gworld,'PyServerTickActor_0',1)
-            camera_actor=ph.FindActorByName(gworld,'CameraActor_1')
-            print('------',tick_actor,camera_actor)
-            ph.MoveToCameraActor(tick_actor,camera_actor)
             #aname='CameraActor_0'
             #aname='CameraActor_0'
             #actor=ph.FindActorByName(gworld,aname,1)
@@ -31,17 +35,18 @@ def main_loop(gworld):
             #print('-----',loc)
             #ph.SetActorLocation(actor,((iter/100)*10,0,0))
 
-        if iter>100 and (iter%1)==0:
+        img=cv2.resize(ph.GetCvScreenshot(),(640,480))
+        if iter>10 and (iter%1)==0:
             #import ipdb;ipdb.set_trace()
-            camera_actor=ph.FindActorByName(gworld,'CameraActor_1')
             loc=ph.GetActorLocation(camera_actor)
             ph.SetActorLocation(camera_actor,(((iter/100)%20)*1+loc[0],loc[1],loc[2]))
-            img=ph.GetCvScreenshot()
             if img is None:
                 print('got None im')
             else:
                 print('got img',img.shape)
-                cv2.imshow('opencv window',img)
+                #import pdb;pdb.set_trace()
+                retimg=of.feed(img)
+                cv2.imshow('opencv window',retimg)
                 cv2.waitKey(1)
 
 def kill():
