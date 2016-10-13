84499cb385210769b8a0dd68fd655711a8ea1401
ori ganoni <ori.ganoni@pg.canterbury.ac.nz>
2016-10-13 16:02:02 +1300

    saving first and last image

--- Source/PyServer/Private/track_test.py
+++ Source/PyServer/Private/track_test.py
@@ -1,6 +1,7 @@
 # vim: tabstop=8 expandtab shiftwidth=4 softtabstop=4
 from Wrappers import phandlers as ph
 import time
+import os,shutil
 import cv2,imp
 import optical_flow
 from optical_flow import optical_flow_track
@@ -11,46 +12,12 @@ cvshow=True
 
 keep_running=True
 
-from queue import Queue
-imgq=Queue()
-
-def cv_loop(imgq):
-    global keep_running
-    of=optical_flow_track()
-    if cvshow:
-        cv2.destroyAllWindows()
-        cv2.waitKey(1)
-    while keep_running:
-        if imgq.empty():
-            yield
-            continue
-        img=imgq.get()
-        if img is None:
-            break
-        if img=='reset':
-            of=optical_flow_track()
-            continue
-        
-        #retimg=img
-        retimg=of.feed(img.copy())
-        if cvshow:
-            cv2.imshow('opencv window',retimg)
-            cv2.waitKey(1)
-    if cvshow:
-        cv2.destroyAllWindows()
-        for _ in range(10): cv2.waitKey(1)
-
-
-cv_loop_itr=None
-proc=None
-
-save_data=True
 
 case_params1={\
 'name':'nowind_low',
 'camera_height': 5,
 'wind_speed':0.00,
-'iterations':2,
+'iterations':1,
 'iteration_frame_cnt': 500,
 'camera_speed':2,
 'frames_in_cycle':400,
@@ -68,15 +35,68 @@ case_params4=case_params2.copy()
 case_params4['name']='wind_heigh'
 case_params4['camera_height']=13
 
-case_params_list=[case_params1,case_params2,case_params3,case_params4]
+#case_params_list=[case_params1,case_params2,case_params3,case_params4]
+case_params_list=[case_params1]
+
+save_path='/tmp/out_ue4'
+
+
+from queue import Queue
+imgq=Queue()
+
+def cv_loop(imgq):
+    global keep_running
+    save_data_path=None
+    of=optical_flow_track()
+    if cvshow:
+        cv2.destroyAllWindows()
+        cv2.waitKey(1)
+    img_cnt=0
+    while keep_running:
+        if imgq.empty():
+            yield
+            continue
+        img=imgq.get()
+        if img is None:
+            break
+        if type(img)==str: #command
+            print('got command: ',img)
+            if img=='reset':
+                save_data_path=None
+                of=optical_flow_track()
+                img_cnt=0
+            if img.startswith('init'):
+                save_data_path=img.split(',')[1]
+                img_cnt=0
+            if img.startswith('save_last'):
+                cv2.imwrite(save_data_path+'/last.png',retimg)
+            continue
+            #retimg=img
+        retimg=of.feed(img.copy())
+        if save_data_path is not None and img_cnt==0: #write first image to disk
+            cv2.imwrite(save_data_path+'/first.png',retimg)
+        if cvshow:
+            cv2.imshow('opencv window',retimg)
+            cv2.waitKey(1)
+        img_cnt+=1
+    if cvshow:
+        cv2.destroyAllWindows()
+        for _ in range(10): cv2.waitKey(1)
+
+
+cv_loop_itr=None
+proc=None
 
 def main_loop(gworld):
     global cv_loop_itr,proc
     
     cv_loop_itr=cv_loop(imgq)
     next(cv_loop_itr)
- 
-    print('-------> start main_loop 2')
+
+    if save_path is not None:
+        if os.path.isdir(save_path):
+            shutil.rmtree(save_path)
+        os.makedirs(save_path)
     camera_actor=ph.FindActorByName(gworld,'CameraActor_2',1) 
     if camera_actor is None:
         print('could not find CameraActor_2 yeilding forever')
@@ -85,6 +105,9 @@ def main_loop(gworld):
     tick_actor=ph.FindActorByName(gworld,'PyServerTickActor_0')
     wind_actor=ph.FindActorByName(gworld,'WindDirectionalSource1') 
     for case_params in case_params_list:
+        if save_path is not None:
+            case_path=save_path+'/'+case_params['name']
+            os.mkdir(case_path)
         for interation_num in range(case_params['iterations']): 
             camera_initial_location=(-370-1000,-2920,case_params['camera_height']*100) #centimeters
             ph.SetActorLocation(camera_actor,camera_initial_location)
@@ -97,7 +120,13 @@ def main_loop(gworld):
             
             ph.MoveToCameraActor(tick_actor,camera_actor)
             
-            imgq.put('reset')
+            if save_path is not None:
+                iter_path=case_path+'/%d'%interation_num
+                os.mkdir(iter_path)
+                imgq.put('init,'+iter_path)
+            else:
+                imgq.put('reset')
+        
 
             for _ in range(300): #adjustment frames...
                 yield
@@ -108,9 +137,9 @@ def main_loop(gworld):
                 cycle=case_params['frames_in_cycle']
                 img=ph.TakeScreenshot() 
                 #img=cv2.resize(img,(640,480))
-                if cnt>cycle:
+                if cnt>=cycle:
                     direction=0
-                elif (cnt%cycle) > cycle/2:
+                elif (cnt%cycle) >= cycle/2:
                     direction=-1
                 else:
                     direction=1
@@ -120,9 +149,13 @@ def main_loop(gworld):
                     print('got None im')
                 else:
                     imgq.put(img)
-                    if not threaded:
-                        next(cv_loop_itr)
+                    next(cv_loop_itr)
                 print('case:',case_params['name'],'iter=',interation_num,r'cnt=',cnt,direction,img.shape,imgq.qsize())
+            if save_path is not None:
+                print('---saving last----')
+                imgq.put('save_last')
+                next(cv_loop_itr)
+
 
     print('Done experiment!')
     while 1:
@@ -131,19 +164,8 @@ def main_loop(gworld):
 def kill():
     global keep_running
     keep_running=False
-    if not threaded:
-        try:
-            for _ in range(1000):
-                next(cv_loop_itr) 
-        except StopIteration:
-            pass
-    else:
-        imgq.put(None)
-        print('---sending None')
-        for _ in range(10):
-            if imgq.qsize()==0:
-                break
-            time.sleep(1)
-        time.sleep(1) 
-        #proc.join()
-        print('---Done kill')
+    try:
+        for _ in range(1000):
+            next(cv_loop_itr) 
+    except StopIteration:
+        pass
