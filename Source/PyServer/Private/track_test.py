c57270b451efd6e91c5f6a338ca66e13b0752179
ori ganoni <ori.ganoni@pg.canterbury.ac.nz>
2016-10-13 14:12:22 +1300

    adding experiments not saving yet

--- Source/PyServer/Private/track_test.py
+++ Source/PyServer/Private/track_test.py
@@ -61,16 +61,29 @@ proc=None
 
 save_data=True
 
-case_params={\
-'fname':'case1',
+case_params1={\
+'name':'nowind_low',
 'camera_height': 5,
 'wind_speed':0.00,
-'iterations':5,
+'iterations':2,
 'iteration_frame_cnt': 500,
 'camera_speed':2,
 'frames_in_cycle':400,
 }
 
+case_params2=case_params1.copy()
+case_params2['name']='wind_low'
+case_params2['wind_speed']=0.1
+
+case_params3=case_params1.copy()
+case_params3['name']='nowind_heigh'
+case_params3['camera_height']=13
+
+case_params4=case_params2.copy()
+case_params4['name']='wind_heigh'
+case_params4['camera_height']=13
+
+case_params_list=[case_params1,case_params2,case_params3,case_params4]
 
 def main_loop(gworld):
     global cv_loop_itr,proc
@@ -98,50 +111,51 @@ def main_loop(gworld):
             yield
     tick_actor=ph.FindActorByName(gworld,'PyServerTickActor_0')
     wind_actor=ph.FindActorByName(gworld,'WindDirectionalSource1') 
-    for interation_num in range(case_params['iterations']): 
-        camera_initial_location=(-370-1000,-2920,case_params['camera_height']*100) #centimeters
-        ph.SetActorLocation(camera_actor,camera_initial_location)
-        ph.SetActorRotation(camera_actor,(-0,-180,-0))
-        
-        #if 1:
-        #    ph.ActivateActor(wind_actor)
-        #else:
-        #    ph.DeactivateActor(wind_actor)
-        ph.SetWindParams(wind_actor,case_params['wind_speed'])
-        
-        #move wind actor otherwize change wind speed doesn't work (https://answers.unrealengine.com/questions/35478/possible-to-change-wind-strength-in-level-blueprin.html)    
-        loc=ph.GetActorLocation(wind_actor)
-        ph.SetActorLocation(wind_actor,(loc[0],loc[1],loc[2]+1000))
-        
-        ph.MoveToCameraActor(tick_actor,camera_actor)
-        
-        imgq.put('reset')
-        #ph.SetScreenResolution(640,480)
+    for case_params in case_params_list:
+        for interation_num in range(case_params['iterations']): 
+            camera_initial_location=(-370-1000,-2920,case_params['camera_height']*100) #centimeters
+            ph.SetActorLocation(camera_actor,camera_initial_location)
+            ph.SetActorRotation(camera_actor,(-0,-180,-0))
+            
+            #if 1:
+            #    ph.ActivateActor(wind_actor)
+            #else:
+            #    ph.DeactivateActor(wind_actor)
+            ph.SetWindParams(wind_actor,case_params['wind_speed'])
+            
+            #move wind actor otherwize change wind speed doesn't work (https://answers.unrealengine.com/questions/35478/possible-to-change-wind-strength-in-level-blueprin.html)    
+            loc=ph.GetActorLocation(wind_actor)
+            ph.SetActorLocation(wind_actor,(loc[0],loc[1],loc[2]+1000))
+            
+            ph.MoveToCameraActor(tick_actor,camera_actor)
+            
+            imgq.put('reset')
+            #ph.SetScreenResolution(640,480)
 
-        for _ in range(100): #adjustment frames...
-            yield
-        for cnt in range(case_params['iteration_frame_cnt']):
-            tic=time.time()
-            yield
-            speed=case_params['camera_speed']
-            cycle=case_params['frames_in_cycle']
-            img=ph.TakeScreenshot() 
-            #img=cv2.resize(img,(640,480))
-            if cnt>cycle:
-                direction=0
-            elif (cnt%cycle) > cycle/2:
-                direction=-1
-            else:
-                direction=1
-            loc=ph.GetActorLocation(camera_actor)
-            ph.SetActorLocation(camera_actor,(direction*speed+loc[0],direction*speed+loc[1],loc[2]))
-            if img is None:
-                print('got None im')
-            else:
-                imgq.put(img)
-                if not threaded:
-                    next(cv_loop_itr)
-            print('iter=',interation_num,r'cnt=',cnt,direction,img.shape,imgq.qsize())
+            for _ in range(100): #adjustment frames...
+                yield
+            for cnt in range(case_params['iteration_frame_cnt']):
+                tic=time.time()
+                yield
+                speed=case_params['camera_speed']
+                cycle=case_params['frames_in_cycle']
+                img=ph.TakeScreenshot() 
+                #img=cv2.resize(img,(640,480))
+                if cnt>cycle:
+                    direction=0
+                elif (cnt%cycle) > cycle/2:
+                    direction=-1
+                else:
+                    direction=1
+                loc=ph.GetActorLocation(camera_actor)
+                ph.SetActorLocation(camera_actor,(direction*speed+loc[0],direction*speed+loc[1],loc[2]))
+                if img is None:
+                    print('got None im')
+                else:
+                    imgq.put(img)
+                    if not threaded:
+                        next(cv_loop_itr)
+                print('case:',case_params['name'],'iter=',interation_num,r'cnt=',cnt,direction,img.shape,imgq.qsize())
 
     print('Done experiment!')
     while 1:
